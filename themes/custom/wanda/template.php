1241db1e44465953ec1a8050bd6f501e8111e860
Jessica Duarte <jessicanduarte@gmail.com>
2014-06-19 11:57:46 -0400

    google fonts

--- themes/custom/wanda/template.php
+++ themes/custom/wanda/template.php
@@ -4,3 +4,7 @@
  * @file
  * template.php
  */
+function wanda_preprocess_html(&$variables) {
+drupal_add_css('http://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700|Great+Vibes', array('type' => 'external'));
+} 
+
