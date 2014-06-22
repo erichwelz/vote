e46658c9c5b3c2cf29cc6a0ad95921a690da5a22
Jessica Duarte <jessicanduarte@gmail.com>
2014-06-22 19:52:20 -0400

    final changes to syle and attempt to prevent default on save button

---
+++ themes/custom/wanda/scripts.js
@@ -0,0 +1,6 @@
+// document.addEventListener("DOMContentLoaded", function() {
+//   // code…
+// document.getElementById('edit-submit').addEventListener('click', function(e){ //
+//         e.preventDefault();
+//    });
+// });
